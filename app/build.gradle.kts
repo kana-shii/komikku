80156ae116d432e1c92dfeaad5780b3c18001ad0
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-03-27 12:28:06 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
     buildTypes {
         val debug by getting {
-            applicationIdSuffix = ".dev"
+            applicationIdSuffix = ".debug"
             versionNameSuffix = "-${getCommitCount()}"
             isPseudoLocalesEnabled = true
         }
